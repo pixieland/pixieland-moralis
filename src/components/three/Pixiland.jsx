4653adc63c7392a645bf3afe0b3a822c6224c70f
Greg DeCarlo <greg@scalabledynamicsllc.com>
2022-11-09 23:42:19 -0500

    Pixiland

--- src/components/three/Pixiland.jsx
+++ src/components/three/Pixiland.jsx
@@ -6,6 +6,7 @@ import { Bounds, Sampler, Edges, Sky, Float, Plane, MeshDistortMaterial, MeshWob
 import { CapsuleCollider, CuboidCollider, RigidBody, useRapier, Physics } from "@react-three/rapier"
 import Perlin from 'perlin.js'
 import { levels } from "./Level"
+import { pixis } from "./Pixi"
 
 Perlin.seed(Math.random())
 
@@ -17,6 +18,8 @@ const rotation = new THREE.Vector3()
 const lightgreen = new THREE.Color('lightgreen')
 const grassMaterial = new THREE.MeshBasicMaterial({ color: lightgreen })
 const grassGeometry = new THREE.ConeGeometry(0.05, 1.0, 2, 20, false, 0, Math.PI)
+
+const randomIndex = (items) => Math.floor(Math.random() * items.length - 1 | 0)
 
 export default function Game() {
     return (
@@ -36,12 +39,18 @@ export default function Game() {
                 <pointLight castShadow intensity={0.8} position={[100, 100, 100]} />
                 <Physics gravity={[0, -30, 0]} colliders={false}>
                     <Ground image={"img/log.png"} />
-                    <Gallery />
-                    <Pixi position={[0, 0, 7]} />
+                    <Gallery images={levels.slice(randomIndex(levels), randomIndex(levels))} />
+                    <Walk position={[0, 2, 17]}>
+                        <Pixi image="img/nft/oracle.png" />
+                    </Walk>
+                    <Follow>
+                        <Pixi image="img/nft/red.png" />
+                    </Follow>
                     <Player>
                         <ImageFrame image={"img/bkg/download (90).jpg"} />
                     </Player>
-                    <Swarm count={1000} />
+                    <Crowd items={Array(200).fill(0).map(n => pixis[randomIndex(pixis)])} />
+                    <Swarm items={Array(200).fill(0).map(n => pixis[randomIndex(pixis)])} />
                 </Physics>
                 <PointerLockControls />
             </Canvas>
@@ -59,51 +68,12 @@ export function Ground({ image, props }) {
     return (
         <RigidBody {...props} type="fixed" colliders={false}>
             {/* <Grass> */}
-                <mesh receiveShadow position={[0, 0, 0]} rotation-x={-Math.PI / 2}>
-                    <planeGeometry args={[1000, 1000]} />
-                    <meshStandardMaterial map={texture} map-repeat={[240, 240]} color="green" />
-                </mesh>
+            <mesh receiveShadow position={[0, 0, 0]} rotation-x={-Math.PI / 2}>
+                <planeGeometry args={[1000, 1000]} />
+                <meshStandardMaterial map={texture} map-repeat={[240, 240]} color="green" />
+            </mesh>
             {/* </Grass> */}
             <CuboidCollider args={[1000, 2, 1000]} position={[0, -2, 0]} />
-        </RigidBody>
-    )
-}
-
-const cubeStore = ({
-    cubes: [],
-    addCube: (x, y, z) => cubeStore.cubes = [...cubeStore.cubes, [x, y, z]]
-})
-
-export function Cube(props) {
-    const ref = useRef()
-    const [hover, set] = useState(null)
-    const texture = useTexture("img/log.png")
-    const onMove = useCallback((e) => {
-        e.stopPropagation()
-        set(Math.floor(e.faceIndex / 2))
-    }, [])
-    const onOut = useCallback(() => set(null), [])
-    const onClick = useCallback((e) => {
-        e.stopPropagation()
-        const { x, y, z } = ref.current.translation()
-        const dir = [
-            [x + 1, y, z],
-            [x - 1, y, z],
-            [x, y + 1, z],
-            [x, y - 1, z],
-            [x, y, z + 1],
-            [x, y, z - 1],
-        ]
-        cubeStore.addCube(...dir[Math.floor(e.faceIndex / 2)])
-    }, [])
-    return (
-        <RigidBody {...props} type="fixed" colliders="cuboid" ref={ref}>
-            <mesh receiveShadow castShadow onPointerMove={onMove} onPointerOut={onOut} onClick={onClick}>
-                {[...Array(6)].map((_, index) => (
-                    <meshStandardMaterial attach={`material-${index}`} key={index} map={texture} color={hover === index ? "hotpink" : "white"} />
-                ))}
-                <boxGeometry />
-            </mesh>
         </RigidBody>
     )
 }
@@ -113,17 +83,14 @@ export function Swarm({ width = 1024, height = 768, children, count = 50, items 
         <group {...props}>
             <mesh position={[0, 25, 0]}>
                 {items.map((item, i) => {
+                    const factor = Math.random() - 0.5
                     const position = item.position || [
-                        //(Math.random() - 0.5) * width,
-                        //(Math.random() + 0.5) * height,
-                        //(Math.random() - 0.5) * width,
-
-                        //(20 + Math.random() * width) * (Math.round(Math.random()) ? -1 : 1),
-                        //-10 + Math.random() * height,
-                        //-5 + Math.random() * 10
+                        factor * width,
+                        1,
+                        factor * width / 3
                     ]
                     //const factor = Perlin.perlin3(position[0], position[1], position[2])
-                    return <Fly key={i} position={position} rotation={[0, position[0] > 0 ? Math.PI : 0, 0]} factor={.5}>
+                    return <Fly key={i} position={position} rotation={[0, position[0] > 0 ? Math.PI : 0, 0]} factor={factor}>
                         <Pixi {...item} />
                     </Fly>
                 })}
@@ -135,7 +102,6 @@ export function Swarm({ width = 1024, height = 768, children, count = 50, items 
 export function Fly({ factor = .5, children, ...props }) {
     const group = useRef()
     const mesh = useRef()
-    //const exp = useExplosion(mesh)
     const [start] = useState(() => Math.random() * 5000)
     const [, key] = useKeyboardControls()
     useFrame((state, delta) => {
@@ -147,7 +113,6 @@ export function Fly({ factor = .5, children, ...props }) {
         mesh.current.position.x = THREE.MathUtils.lerp(mesh.current.position.x, t * 50, factor)
         mesh.current.position.y = THREE.MathUtils.lerp(mesh.current.position.y, t * 50, factor)
         mesh.current.position.z = THREE.MathUtils.lerp(mesh.current.position.z, t * 10, factor)
-        //if (shoot) exp.start()
     })
     return (
         <group ref={group} {...props}>
@@ -160,7 +125,92 @@ export function Fly({ factor = .5, children, ...props }) {
     )
 }
 
-export function Pixi({ image = "img/nft/red.png", power = 100, factor = .1, color = "hotpink", ...props }) {
+export function Crowd({ width = 1024, height = 768, children, count = 50, items = Array(count).fill({}), ...props }) {
+    return (
+        <group {...props}>
+            <mesh position={[0, 0, 0]}>
+                {items.map((item, i) => {
+                    const position = item.position || [
+                        (Math.random() - 0.5) * width,
+                        1,
+                        (Math.random() - 0.5) * height
+                    ]
+                    const to = [
+                        (Math.random() - 0.5) * width,
+                        1,
+                        (Math.random() - 0.5) * height
+                    ]
+                    //factor = Perlin.perlin3(position[0], position[1], position[2])
+                    return <Walk key={i} position={position} rotation={[0, position[0] > 0 ? Math.PI : 0, 0]} to={to} factor={.05}>
+                        <Pixi {...item} />
+                    </Walk>
+                })}
+            </mesh>
+        </group>
+    )
+}
+
+export function Walk({ to = [
+    (Math.random() - 0.5) * 1024,
+    1,
+    (Math.random() - 0.5) * 768
+], factor = .01, children, ...props }) {
+    const group = useRef()
+    const mesh = useRef()
+    const [start] = useState(() => Math.random() * 5000)
+    useFrame((state, delta) => {
+        const t = Math.sin(start + state.clock.elapsedTime)
+        mesh.current.rotation.y = (t * Math.PI) / 2
+        //const p = Perlin.perlin3(mesh.current.position.x, mesh.current.position.y, mesh.current.position.z)
+        rotation.set(to[0], to[1], to[2])
+        if(rotation.distanceTo(mesh.current.position) > 1) {
+            mesh.current.position.x = THREE.MathUtils.lerp(mesh.current.position.x, to[0], factor)
+            mesh.current.position.z = THREE.MathUtils.lerp(mesh.current.position.z, to[2], factor)
+        } else {
+            mesh.current.position.x = THREE.MathUtils.lerp(to[0], mesh.current.position.x, factor)
+            mesh.current.position.z = THREE.MathUtils.lerp(to[2], mesh.current.position.z, factor)
+        }
+    })
+    return (
+        <group ref={mesh} {...props} position-y={2}>
+            {children}
+        </group>
+    )
+}
+
+const getNewPointOnVector = (p1, p2) => {
+    let distAway = 200;
+    let vector = { x: p2.x - p1.x, y: p2.y - p1.y, z: p2.z - p1.z };
+    let vl = Math.sqrt(Math.pow(vector.x, 2) + Math.pow(vector.y, 2) + Math.pow(vector.z, 2));
+    let vectorLength = { x: vector.x / vl, y: vector.y / vl, z: vector.z / vl };
+    let v = { x: distAway * vectorLength.x, y: distAway * vectorLength.y, z: distAway * vectorLength.z };
+    return { x: p2.x + v.x, y: p2.y + v.y, z: p2.z + v.z };
+}
+
+export function Follow({ factor = .01, children, ...props }) {
+    const group = useRef()
+    const mesh = useRef()
+    const [start] = useState(() => Math.random() * 5000)
+    const { camera } = useThree()
+    useFrame(() => {
+        group.current.lookAt(camera.position)
+        const position = getNewPointOnVector(camera.position, group.current.position)
+        group.current.position.x = THREE.MathUtils.lerp(group.current.position.x, position.x, factor)
+        group.current.position.y = THREE.MathUtils.lerp(group.current.position.y, position.y, factor)
+        group.current.position.z = THREE.MathUtils.lerp(group.current.position.z, position.z, factor)
+        //group.current.rotation.y = (t * Math.PI) / 2
+    })
+    return (
+        <group ref={group} position-y={2} {...props}>
+            <RigidBody ref={mesh} colliders={false} mass={1} type="dynamic" position={[0, 10, 0]} enabledRotations={[false, false, false]}>
+                <CapsuleCollider args={[0.75, 0.5]} />
+            </RigidBody>
+            {children}
+        </group>
+    )
+}
+
+export function Pixi({ image = "img/nft/red.png", power = 100, factor = .1, color = "hotpink", double, ...props }) {
     const texture = useTexture(image)
     return (
         <mesh {...props}>
@@ -176,17 +226,19 @@ export function Pixi({ image = "img/nft/red.png", power = 100, factor = .1, colo
                         side={THREE.DoubleSide}
                     />
                 </Plane>
-                <Plane args={[5, 5]} rotation-y={.5}>
-                    <MeshWobbleMaterial
-                        attach="material"
-                        factor={factor} // Strength, 0 disables the effect (default=1)
-                        speed={SPEED} // Speed (default=1)
-                        roughness={0}
-                        map={texture}
-                        transparent
-                        side={THREE.DoubleSide}
-                    />
-                </Plane>
+                {double &&
+                    <Plane args={[5, 5]} rotation-y={.5}>
+                        <MeshWobbleMaterial
+                            attach="material"
+                            factor={factor} // Strength, 0 disables the effect (default=1)
+                            speed={SPEED} // Speed (default=1)
+                            roughness={0}
+                            map={texture}
+                            transparent
+                            side={THREE.DoubleSide}
+                        />
+                    </Plane>
+                }
             </Float>
             <Sparkles count={power} scale={[5, 5, .2]} size={7} color={color} opacity={.5} />
         </mesh>
@@ -273,19 +325,12 @@ export function Gallery({ items = Array(10).fill("img/forest.png"), radius = ite
     )
 }
 
-function ImageFrame({ image, q = new THREE.Quaternion(), p = new THREE.Vector3(), ...props }) {
+function ImageFrame({ image, ...props }) {
     const texture = useTexture(image)
     const { camera } = useThree()
     const frame = useRef()
-    useEffect(() => {
-        p.set(0, 0, 5.5)
-        q.identity()
-    })
-    useFrame((state, dt) => {
+    useFrame(() => {
         frame.current.lookAt(camera.position)
-        //rotation.y = Math.min(0,camera.rotation.y - (Math.PI / 2.5))
-        //frame.current.getWorldPosition(p)
-        //frame.current.getWorldQuaternion(q)
     })
     return (
 
@@ -293,14 +338,6 @@ function ImageFrame({ image, q = new THREE.Quaternion(), p = new THREE.Vector3()
             <mesh>
                 <planeGeometry args={[1100, 1100, 5, 5]} />
                 <meshStandardMaterial color="Goldenrod" side={THREE.DoubleSide} />
-                {/* <MeshWobbleMaterial
-                    attach="material"
-                    factor={.03} // Strength, 0 disables the effect (default=1)
-                    speed={2} // Speed (default=1)
-                    roughness={2}
-                    color="Goldenrod"
-                    side={THREE.DoubleSide}
-                /> */}
             </mesh>
             <mesh position={[0, 5, 10]}>
                 <planeGeometry args={[1024, 768, 1, 1]} />
